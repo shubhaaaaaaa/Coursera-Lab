0dcded5f0ad027f952fa4ccf74756044b5584f27
shubhaaaaaaa <shubhakhadgi007@gmail.com>
2024-06-06 22:59:42 +0545

    LSPP Day 6

--- JS by Programiz/Objects.js
+++ JS by Programiz/Objects.js
@@ -36,3 +36,38 @@ function Dog(name, breed, location){
 let Dog1 = new Dog('Junu', 'Cocker spaniel', 'Gyaneshowr');
 console.log(Dog1.name);
 
+//Types of object properties
+
+let myDog = {
+    breed: 'Japanese spitz',                       //Data properties
+    color: 'white',
+
+    get getBreed(){                               //Accessor properties (methods use to get or set value of an object)
+    return this.breed;
+    },
+
+    set setBreed(newBreed){
+        this.breed = newBreed;
+    }
+}
+
+myDog.setBreed = 'German Shepherd';              //calling as properties not function
+console.log(myDog.getBreed);
+
+Object.defineProperty(myDog, 'getColor', {       //using defineProperty
+    get: function(){
+        return this.color;
+    }
+})
+
+Object.defineProperty(myDog, 'setColor', {
+    set : function(newcolor){
+        this.color = newcolor;
+    }
+})
+
+myDog.setColor = 'red';
+console.log(myDog.getColor);
+
+
+
